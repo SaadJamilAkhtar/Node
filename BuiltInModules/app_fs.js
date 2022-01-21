e3c26119cefc45387e1f3dddc8657ffffe31f6b5
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-21 06:54:21 +0500

    fs module sync

---
+++ BuiltInModules/app_fs.js
@@ -0,0 +1,14 @@
+const {readFileSync, writeFileSync} = require('fs')
+
+const file1 = './test1.txt'
+const file2 = './test2.txt'
+
+const first = readFileSync(file1, 'utf8')
+const second = readFileSync(file2, 'utf8')
+
+console.log(first)
+console.log(second)
+
+const newFile = writeFileSync("newFile.txt", "Hello from newFile")
+
+console.log(readFileSync("./newFile.txt", 'utf8'))
