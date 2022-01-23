8466998957e52ce00759565fe0393002f5cadd17
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-23 10:10:24 +0500

    async code example 2 ( issue still unresolved )

---
+++ AsyncCode2.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    setTimeout(() => {
+        setTimeout(() => {
+            // Blocking code
+            for (let i = 0; i < 1000; i++) {
+                for (let j = 0; j < 1000; j++) {
+                    console.log(` ${i} ${j}`)
+                }
+            }
+        }, 0);
+
+
+        res.end("<h1> Hello World! </h1>")
+    }, 0);
+
+})
+
+server.listen(5000, () => {
+    console.log("Server listening on port 5000 ...")
+})
