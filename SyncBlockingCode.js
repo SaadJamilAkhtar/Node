0fb5e60444326b9e67e44fa397ec5f5a36c4da46
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-23 10:05:28 +0500

    testing sync code

---
+++ SyncBlockingCode.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    // Blocking code
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            console.log(` ${i} ${j}`)
+        }
+    }
+
+    res.end("<h1> Hello World! </h1>")
+})
+
+server.listen(5000, () => {
+    console.log("Server listening on port 5000 ...")
+})
