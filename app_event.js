be9e9667d77c1623459f79f71076bcd279bcea32
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-25 09:59:58 +0500

    events practice 1 - complete

--- app_event.js
+++ app_event.js
@@ -3,10 +3,15 @@ const EventEmitter = require("events");
 const customEmitter = new EventEmitter()
 
 customEmitter.on("response", () => {
-    console.log(`data received `)
-});
-customEmitter.on("response", () => {
     console.log(`Different logic `)
 });
 
-customEmitter.emit("response");
+customEmitter.on("response", () => {
+    console.log(`data received `)
+});
+
+customEmitter.on("response", (name, num) => {
+    console.log(`data received ${num} : ${name}`)
+});
+
+customEmitter.emit("response", "John", 34);
