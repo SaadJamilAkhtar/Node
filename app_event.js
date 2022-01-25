6f8471f1ba1029f04b70b73796853493644c28fe
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-25 09:57:26 +0500

    events 2

--- app_event.js
+++ app_event.js
@@ -5,5 +5,8 @@ const customEmitter = new EventEmitter()
 customEmitter.on("response", () => {
     console.log(`data received `)
 });
+customEmitter.on("response", () => {
+    console.log(`Different logic `)
+});
 
 customEmitter.emit("response");
