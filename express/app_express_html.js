8357bc14e71494005532775daf5beb20999225df
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-02 00:48:29 +0500

    added public files to express

--- express/app_express_html.js
+++ express/app_express_html.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use(express.static("./public"))
+
 app.get("/", (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname,"./index.html"))
+    res.status(200).sendFile(path.resolve(__dirname, "./index.html"))
 })
 
-app.all("*", (req,res) => {
+app.all("*", (req, res) => {
     res.status(404).send("404 Page not found!")
 })
 
