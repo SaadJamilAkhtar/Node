24c4363b53af62630c02a76975c2af9f8cac804a
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-02 00:50:13 +0500

    updated express html file with comments

--- express/app_express_html.js
+++ express/app_express_html.js
@@ -2,16 +2,20 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// setup static and middleware
 app.use(express.static("./public"))
 
+// add index page
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, "./index.html"))
 })
 
+// handle 4040
 app.all("*", (req, res) => {
     res.status(404).send("404 Page not found!")
 })
 
+// start app
 app.listen(5000, () => {
     console.log("server listening at port 5000...")
 })
