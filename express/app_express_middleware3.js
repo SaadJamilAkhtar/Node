8eda93c97b0b511f4fbf0850143c64823b2bee2b
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-06 18:50:13 +0500

    express middleware [apply to specific routes ]

---
+++ express/app_express_middleware3.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const logger = require('./middleware')
+
+app.use("/api", logger)
+
+app.get('/', (req, res) => {
+    res.send("<h1>home</h1>")
+})
+
+app.get('/about', (req, res) => {
+    res.send("<h1>About</h1>")
+})
+
+app.get('/api/about', (req, res) => {
+    res.send("<h1>API About</h1>")
+})
+
+app.get('/api/home', (req, res) => {
+    res.send("<h1>API Home</h1>")
+})
+
+app.listen(5000, () => {
+    console.log("Server listening at port 5000...")
+})
